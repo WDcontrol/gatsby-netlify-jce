d5dd39800699c4286648575797ff263c39d60ab2
unknown <jerome.celebi@ynov.com>
2020-04-24 17:13:41 +0200

    adding success and cancel url

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -8,18 +8,19 @@ const Index = () => {
     e.preventDefault()
 
     const stripe = await stripePromise
-    stripe
-      .redirectToCheckout({
-        items: [
-          {
-            sku: "sku_H9mYBvTf8kT9Rj",
-            quantity: 1,
-          },
-        ],
-        successUrl: "https://zealous-clarke-80bed4.netlify.app/success/",
-        cancelUrl: "https://zealous-clarke-80bed4.netlify.app/fail/",
-      })
-      .then(res => {})
+
+    stripe.redirectToCheckout({
+      items: [
+        {
+          // Define the product and SKU in the Dashboard first, and use the SKU
+          // ID in your client-side code.
+          sku: "sku_H9mYBvTf8kT9Rj",
+          quantity: 1,
+        },
+      ],
+      successUrl: "https://www.example.com/success",
+      cancelUrl: "https://www.example.com/cancel",
+    })
   }
 
   return (
