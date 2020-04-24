9afca76b9d848ae5b8a84c45bb7ee8ef9c9bea87
unknown <jerome.celebi@ynov.com>
2020-04-24 16:43:53 +0200

    test stripe

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,15 +1,31 @@
 import React, { useState } from "react"
 
+import { loadStripe } from "@stripe/stripe-js"
+
 const Index = () => {
-  const submitForm = () => {
+  const stripePromise = loadStripe("pk_test_0T41rHaGWQh5lb1ZxBa9Duuh00W7nRfNzd")
+  const checkoutButton = async e => {
     e.preventDefault()
-    console.log(inputForm)
+
+    const stripe = await stripePromise
+    stripe
+      .redirectToCheckout({
+        items: [
+          {
+            sku: "sku_H9mHAmJ32Mf9uQ",
+            quantity: 1,
+          },
+        ],
+        successUrl: "",
+        cancelUrl: "",
+      })
+      .then(res => {})
   }
 
   return (
     <div>
       <form onSubmit={submitForm}>
-        <button type="submit">S'inscrire</button>
+        <button type="submit">Payer</button>
       </form>
     </div>
   )
