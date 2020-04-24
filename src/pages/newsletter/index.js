4d0347b651745b6bb1b4ce038a34cd954668b0ba
unknown <jerome.celebi@ynov.com>
2020-04-24 16:52:56 +0200

    test

--- src/pages/newsletter/index.js
+++ src/pages/newsletter/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import axios from "axios"
 
 const Index = () => {
   const [inputForm, setInputForm] = useState({
@@ -15,31 +14,22 @@ const Index = () => {
   const submitForm = e => {
     e.preventDefault()
     console.log(inputForm)
-    axios
-      .post(
-        `https://zealous-clarke-80bed4.netlify.app/.netlify/functions/subscibe?mail=${inputForm.email}`
-      )
-      .then(res => {
-        console.log(res)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-
-    return (
-      <div>
-        <form onSubmit={submitForm}>
-          <input
-            type="email"
-            value={inputForm.email}
-            name="email"
-            onChange={handleChange}
-          />
-          <button type="submit">S'inscrire</button>
-        </form>
-      </div>
-    )
+    await
   }
+
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <input
+          type="email"
+          value={inputForm.email}
+          name="email"
+          onChange={handleChange}
+        />
+        <button type="submit">S'inscrire</button>
+      </form>
+    </div>
+  )
 }
 
 export default Index
