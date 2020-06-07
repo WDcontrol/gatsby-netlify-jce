4002876e0e5d5aab3de0ec1cb18adf127f133a99
Jérôme CELEBI <jerome.celebi@macbook-pro-de-jerome.home>
2020-06-08 01:34:19 +0200

    context ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import { navigate, Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import IdentityModal, {
   useIdentityContext,
 } from "react-netlify-identity-widget"
@@ -10,6 +10,9 @@ import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
 import { CartContext } from "../../context/cartContext"
 
 const Header = () => {
+  const cart = useContext(CartContext)
+  const cartSize = Object.keys(cart.cart).length
+
   const identity = useIdentityContext()
   const [dialog, setDialog] = useState(false)
   const name =
@@ -36,48 +39,38 @@ const Header = () => {
     }
     return <option></option>
   }
-
   return (
-    <CartContext.Consumer>
-      {cart => {
-        const cartSize = Object.keys(cart.cart).length
-        return (
-          <header>
-            {JSON.stringify(cart)}
-            <nav className="header__nav">
-              <Link className="nav__main-link" to="/">
-                Klous
-              </Link>
-              <div className="nav__select-container">
-                <select onChange={onChange} className="nav__select">
-                  <CustomOption value="/" label="Home" />
-                  <CustomOption value="/products" label="Products" />
-                  <CustomOption value="/contact" label="Contacts" />
-                  <CustomOption value="/blog" label="Blog" />
-                  <CustomOption value="/profile" label="Profile" />
-                  <CustomOption value="/cart" label="Cart" />
-                </select>
-              </div>
-              <div className="nav__section">
-                <div className="nav__link" onClick={() => setDialog(true)}>
-                  {isLoggedIn ? `${name}` : `Log In`}
-                </div>
-                <Link to="/cart">
-                  <ShoppingBasketIcon className="nav__link" />
-                  {cartSize > 0 && (
-                    <div className="nav__link_notif">{cartSize}</div>
-                  )}
-                </Link>
-              </div>
-            </nav>
-            <IdentityModal
-              showDialog={dialog}
-              onCloseDialog={() => setDialog(false)}
-            />
-          </header>
-        )
-      }}
-    </CartContext.Consumer>
+    <header>
+      {JSON.stringify(cart)}
+      <nav className="header__nav">
+        <Link className="nav__main-link" to="/">
+          Klous
+        </Link>
+        <div className="nav__select-container">
+          <select onChange={onChange} className="nav__select">
+            <CustomOption value="/" label="Home" />
+            <CustomOption value="/products" label="Products" />
+            <CustomOption value="/contact" label="Contacts" />
+            <CustomOption value="/blog" label="Blog" />
+            <CustomOption value="/profile" label="Profile" />
+            <CustomOption value="/cart" label="Cart" />
+          </select>
+        </div>
+        <div className="nav__section">
+          <div className="nav__link" onClick={() => setDialog(true)}>
+            {isLoggedIn ? `${name}` : `Log In`}
+          </div>
+          <Link to="/cart">
+            <ShoppingBasketIcon className="nav__link" />
+            {cartSize > 0 && <div className="nav__link_notif">{cartSize}</div>}
+          </Link>
+        </div>
+      </nav>
+      <IdentityModal
+        showDialog={dialog}
+        onCloseDialog={() => setDialog(false)}
+      />
+    </header>
   )
 }
 
--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,8 +1,19 @@
 import React, { useContext } from "react"
 import Layout from "../../components/Layout"
+import { CartContext } from "../../context/cartContext"
 
 const Cart = () => {
-  return <Layout>{1}</Layout>
+  const cart = useContext(CartContext)
+  return (
+    <Layout>
+      {Object.keys(cart.cart).map(elem => (
+        <>
+          <h1>{cart.cart[elem]}</h1>
+          <h1>{elem}</h1>
+        </>
+      ))}
+    </Layout>
+  )
 }
 
 export default Cart
